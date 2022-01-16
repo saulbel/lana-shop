33dfaf482747b99fd34d17ba2e18260ea564e007
Saulbel <saulitobel@gmail.com>
2022-01-16 18:38:42 +0100

    v1.0

--- app.py
+++ app.py
@@ -37,7 +37,6 @@ def addProductToBasket():
     if (add_product["item"] != 'PEN' and add_product["item"] != 'TSHIRT' and add_product["item"] != 'MUG'):
         return jsonify({"LANA'S SHOP": "Product not available"}, {"Basket": basket})
     else:
-        print(add_product["item"])
         basket.append(add_product)
         return jsonify({"LANA'S SHOP": "Product added successfully to basket"}, {"Basket": basket})
 
@@ -45,16 +44,23 @@ def addProductToBasket():
 # GET for calculating total price
 @app.route('/totalbasket', methods=['GET'])
 def getTotalBasket():
+    # total items in basket
     totalTshirt = sum([i['item'] == "TSHIRT" for i in basket])
     totalPen = sum([i['item'] == "PEN" for i in basket])
     totalMug = sum([i['item'] == "MUG" for i in basket])
-    totalPrice = float(totalTshirt * 20) + float(totalPen * 5) + float(totalMug * 7.5)
+    # products prices
+    penPrice = products[0]['price']
+    tshirtPrice = products[1]['price']
+    tshirtReducedPrice = (products[1]['price'] * 25) / 100
+    mugPrice = products[2]['price']
+    # total price of all items in basket
+    totalPrice = float(totalTshirt * tshirtPrice) + float(totalPen * penPrice) + float(totalMug * mugPrice)
     # 2x1 free promotion, it only applies ONCE
     if (totalPen >= 2):
-        totalPrice = totalPrice - 5
+        totalPrice = totalPrice - penPrice
     # 25% of disscount in each Tshirt if 3 or more
     if (totalTshirt >= 3):
-        totalPrice = totalPrice - (totalTshirt * 5)
+        totalPrice = totalPrice - (totalTshirt * tshirtReducedPrice)
     return jsonify({"LANA'S SHOP": "This is your invoice"}, {"Basket": basket}, {"Total": totalPrice})
 
 
